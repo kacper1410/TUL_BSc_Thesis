1d2ab386b3d7416b5ccfcb8c97d0e83be01ecd5f
Kacper <kacper.swiercz00@gmail.com>
2022-05-29 18:44:29 +0200

    [Backend] Fix confirmation email link

--- backend/bookmind/src/main/java/tul/swiercz/thesis/bookmind/service/MailService.java
+++ backend/bookmind/src/main/java/tul/swiercz/thesis/bookmind/service/MailService.java
@@ -1,6 +1,7 @@
 package tul.swiercz.thesis.bookmind.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -10,15 +11,19 @@ public class MailService {
 
     private final JavaMailSender javaMailSender;
 
+    @Value("${application.frontend.url}")
+    private String frontUrl;
+
     @Autowired
     public MailService(JavaMailSender javaMailSender) {
         this.javaMailSender = javaMailSender;
     }
 
-    public void sendRegisterConfirmation(String recipient, String url) {
+    public void sendRegisterConfirmation(String recipient, String code) {
+        String confirmationUrl = frontUrl + "/confirm/" + code;
         SimpleMailMessage message = getDefaultMessage(recipient);
         message.setSubject("Confirmation email");
-        message.setText("To confirm your email, please click link below\n" + url);
+        message.setText("To confirm your email, please click link below\n" + confirmationUrl);
         javaMailSender.send(message);
     }
 
