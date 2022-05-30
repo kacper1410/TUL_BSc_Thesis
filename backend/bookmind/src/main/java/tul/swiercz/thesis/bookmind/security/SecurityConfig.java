282e4631f5ed47e9be37140089d59443a3070cf0
Kacper <kacper.swiercz00@gmail.com>
2022-05-30 19:18:04 +0200

    [Backend] Force secure connection

--- backend/bookmind/src/main/java/tul/swiercz/thesis/bookmind/security/SecurityConfig.java
+++ backend/bookmind/src/main/java/tul/swiercz/thesis/bookmind/security/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.requiresChannel().anyRequest().requiresSecure();
 
         http.cors().and()
                 .csrf().disable()
